a3887bc861362c0225e84e4c82668ffac7fd8952
Enry Seitz <enrique6tz@gmail.com>
2023-04-11 18:24:28 -0300

    frist commit

---
+++ js/administrarProductos.js
@@ -0,0 +1,222 @@
+let listaProductos = [{
+    id: 1,
+    nombre: "Air Pods",
+    precio: 85000,
+    stock: 13,
+    descripcion: "Los AirPods son los auriculares inalámbricos más populares del mercado. Diseñados por Apple, ofrecen una experiencia auditiva de alta calidad y una conectividad sencilla con dispositivos compatibles mediante Bluetooth. ",
+    cantidad: 1,
+    img: "./assets/iphone/auriculares/1.png",
+    alt: "Air Pods"
+}, {
+    id: 2,
+    nombre: "Air Pods PRO",
+    precio: 105000,
+    stock: 10,
+    descripcion: "Los Apple AirPods Pro son unos auriculares inalámbricos con un diseño ergonómico y resistente al sudor y agua. Ofrecen cancelación activa de ruido para una experiencia auditiva inmersiva y un modo de sonido ambiente para estar más consciente del entorno. Se conectan a dispositivos compatibles mediante Bluetooth y tienen una batería de larga duración que se puede recargar en su estuche compacto.",
+    cantidad: 1,
+    img: "./assets/iphone/auriculares/2.jpg",
+    alt: "Air Pods PRO"
+}, {
+    id: 3,
+    nombre: "Air Pods MAX",
+    precio: 98000,
+    stock: 12,
+    descripcion: "Para los auriculares de vincha de iPhone, los Apple AirPods Max son una excelente opción. Con una calidad de sonido superior, los AirPods Max cuentan con cancelación activa de ruido y un diseño elegante y cómodo para largas sesiones de escucha.",
+    cantidad: 1,
+    img: "./assets/iphone/auriculares/3.jpg",
+    alt: "Air Pods MAX"
+}, {
+    id: 4,
+    nombre: "Iphone 12",
+    precio: 250000,
+    stock: 8,
+    descripcion: "Con un diseño renovado y elegante, cuenta con una pantalla OLED Super Retina XDR de borde a borde de 6,1 pulgadas, una cámara dual de 12 megapíxeles, y es compatible con la red 5G para una conexión de alta velocidad",
+    cantidad: 1,
+    img: "./assets/iphone/celular/1.png",
+    alt: "Iphone 12"
+}, {
+    id: 5,
+    nombre: "Iphone 13",
+    precio: 3000000,
+    stock: 5,
+    descripcion: "Con un diseño elegante y moderno, el iPhone 13 cuenta con una pantalla OLED Super Retina XDR de alta resolución de borde a borde de 6.1 pulgadas y una cámara mejorada con capacidad de grabación de video en 8K",
+    cantidad: 1,
+    img: "./assets/iphone/celular/2.png",
+    alt: "Iphone 13"
+}, {
+    id: 6,
+    nombre: "Iphone 14 Pro",
+    precio: 375000,
+    stock: 7,
+    descripcion: "El iPhone 14 Pro es un teléfono muy bueno que ofrece cámaras mejoradas, un chip A15 Bionic más rápido y un modo de acción divertido para la captura de video.",
+    cantidad: 1,
+    img: "./assets/iphone/celular/3.jpg",
+    alt: "Iphone 14 Pro"
+}, {
+    id: 7,
+    nombre: " Apple Watch  7 - Dorado ",
+    precio: 77000,
+    stock: 7,
+    descripcion: "El Apple Watch 7 es un reloj inteligente de última generación con un diseño elegante y moderno y una amplia variedad de características mejoradas, incluyendo una pantalla más grande y brillante, mayor duración de la batería, resistencia al agua mejorada, y mayor capacidad de almacenamiento interno. Es el compañero perfecto para aquellos que buscan un estilo de vida activo y quieren monitorear su actividad física y bienestar, todo en un dispositivo portátil y fácil de usar.",
+    cantidad: 1,
+    img: "./assets/iphone/reloj/1.jpeg",
+    alt: "Apple Watch  7 - Dorado "
+}, {
+    id: 8,
+    nombre: "Apple Watch  7 - Plateado",
+    precio: 77000,
+    stock: 4,
+    descripcion: "El Apple Watch 7 es un reloj inteligente de última generación con un diseño elegante y moderno y una amplia variedad de características mejoradas, incluyendo una pantalla más grande y brillante, mayor duración de la batería, resistencia al agua mejorada, y mayor capacidad de almacenamiento interno. Es el compañero perfecto para aquellos que buscan un estilo de vida activo y quieren monitorear su actividad física y bienestar, todo en un dispositivo portátil y fácil de usar.",
+    cantidad: 1,
+    img: "./assets/iphone/reloj/2.jpg",
+    alt: "Apple Watch  7 - Plateado"
+}, {
+    id: 9,
+    nombre: "Apple Watch  7 - Negro ",
+    precio: 77000,
+    stock: 9,
+    descripcion: "El Apple Watch 7 es un reloj inteligente de última generación con un diseño elegante y moderno y una amplia variedad de características mejoradas, incluyendo una pantalla más grande y brillante, mayor duración de la batería, resistencia al agua mejorada, y mayor capacidad de almacenamiento interno. Es el compañero perfecto para aquellos que buscan un estilo de vida activo y quieren monitorear su actividad física y bienestar, todo en un dispositivo portátil y fácil de usar.",
+    cantidad: 1,
+    img: "./assets/iphone/reloj/3.jpg",
+    alt: "Apple Watch  7 - Negro "
+}, {
+    id: 10,
+    nombre: "Apple iPad Pro 11",
+    precio: 400000,
+    stock: 10,
+    descripcion: "El iPad Pro es la tablet perfecta para aquellos que buscan un dispositivo de alta calidad y rendimiento. Con su pantalla de alta resolución, procesador avanzado y compatibilidad con el Apple Pencil y el Magic Keyboard, el iPad Pro ofrece una experiencia de productividad y entretenimiento de primer nivel. Ya sea para trabajar, estudiar o simplemente disfrutar de juegos y contenido multimedia, el iPad Pro es el dispositivo portátil ideal para llevar a cualquier lugar.",
+    cantidad: 1,
+    img: "./assets/iphone/tablet/1.jpg",
+    alt: "Apple iPad Pro 11"
+}, {
+    id: 11,
+    nombre: "Apple iPad 9 gen 10.2 ",
+    precio: 225000,
+    stock: 13,
+    descripcion: "El Apple iPad 9 gen 10.2 es una tablet asequible pero poderosa que ofrece una experiencia de usuario excepcional. Con una pantalla Retina de 10.2 pulgadas, un chip A13 Bionic y conectividad Wi-Fi y LTE, el iPad 9 gen es capaz de manejar fácilmente las tareas diarias y el entretenimiento, mientras que su duradera batería permite un uso prolongado. Además, es compatible con el Apple Pencil y el Smart Keyboard, lo que lo convierte en una herramienta de productividad versátil para el trabajo y el estudio.",
+    cantidad: 1,
+    img: "./assets/iphone/tablet/3.jpg",
+    alt: "Apple iPad 9 gen 10.2"
+}, {
+    id: 12,
+    nombre: "Apple iPad mini 6 gen 8.3",
+    precio: 250000,
+    stock: 2,
+    descripcion: "El Apple iPad mini 6 gen 8.3 es una tablet compacta pero potente que ofrece una experiencia de usuario impresionante. Con una pantalla Liquid Retina de 8.3 pulgadas, un chip A15 Bionic y conectividad Wi-Fi y LTE, el iPad mini 6 es capaz de manejar fácilmente las tareas diarias y el entretenimiento, mientras que su diseño ultraportátil lo hace perfecto para llevar a cualquier lugar. Además, es compatible con el Apple Pencil de segunda generación y es compatible con el Smart Keyboard, lo que lo convierte en una herramienta de productividad versátil para el trabajo y el estudio.",
+    cantidad: 1,
+    img: "./assets/iphone/tablet/2.jpg",
+    alt: "Apple iPad mini 6 gen 8.3"
+}, {
+    id: 13,
+    nombre: "Samsung Buds Blanco",
+    precio: 47000,
+    stock: 13,
+    descripcion: "Los Samsung Buds son unos auriculares inalámbricos de alta calidad con un diseño ergonómico y compacto que se ajusta cómodamente en el oído.  Se conectan a dispositivos compatibles mediante Bluetooth y tienen una batería de larga duración que se puede recargar en su estuche compacto.",
+    cantidad: 1,
+    img: "./assets/samsung/auriculares/1.png",
+    alt: "Samsung Buds Blanco"
+}, {
+    id: 14,
+    nombre: "Samsung Buds 2",
+    precio: 55000,
+    stock: 10,
+    descripcion: "Los Samsung Buds 2 son unos auriculares inalámbricos de alta calidad con un diseño ergonómico y compacto que se ajusta cómodamente en el oído. Con una conexión Bluetooth estable y una batería de larga duración, los Buds ofrecen una experiencia de audio excepcional con una calidad de sonido clara y rica.",
+    cantidad: 1,
+    img: "./assets/samsung/auriculares/2.png",
+    alt: "Samsung Buds 2"
+}, {
+    id: 15,
+    nombre: "Samsung Buds 2 PRO",
+    precio: 65000,
+    stock: 12,
+    descripcion: "Los Samsung Buds 2 PRO son unos auriculares inalámbricos de alta calidad con un diseño ergonómico y compacto que se ajusta cómodamente en el oído. Con una conexión Bluetooth estable y una batería de larga duración, los Buds ofrecen una experiencia de audio excepcional con una calidad de sonido clara y rica.",
+    cantidad: 1,
+    img: "./assets/samsung/auriculares/3.png",
+    alt: "Samsung Buds 2 PRO"
+}, {
+    id: 16,
+    nombre: "Iphone 12",
+    precio: 250000,
+    stock: 8,
+    descripcion: "Con un diseño renovado y elegante, cuenta con una pantalla OLED Super Retina XDR de borde a borde de 6,1 pulgadas, una cámara dual de 12 megapíxeles, y es compatible con la red 5G para una conexión de alta velocidad",
+    cantidad: 1,
+    img: "./assets/samsung/celular/1.png",
+    alt: "Iphone 12"
+}, {
+    id: 17,
+    nombre: "Iphone 13",
+    precio: 3000000,
+    stock: 5,
+    descripcion: "Con un diseño elegante y moderno, el iPhone 13 cuenta con una pantalla OLED Super Retina XDR de alta resolución de borde a borde de 6.1 pulgadas y una cámara mejorada con capacidad de grabación de video en 8K",
+    cantidad: 1,
+    img: "./assets/samsung/celular/2.png",
+    alt: "Iphone 13"
+}, {
+    id: 18,
+    nombre: "Iphone 14 Pro",
+    precio: 375000,
+    stock: 7,
+    descripcion: "El iPhone 14 Pro es un teléfono muy bueno que ofrece cámaras mejoradas, un chip A15 Bionic más rápido y un modo de acción divertido para la captura de video.",
+    cantidad: 1,
+    img: "./assets/samsung/celular/3.png",
+    alt: "Iphone 14 Pro"
+}, {
+    id: 19,
+    nombre: " Apple Watch  7 - Dorado ",
+    precio: 77000,
+    stock: 7,
+    descripcion: "El Apple Watch 7 es un reloj inteligente de última generación con un diseño elegante y moderno y una amplia variedad de características mejoradas, incluyendo una pantalla más grande y brillante, mayor duración de la batería, resistencia al agua mejorada, y mayor capacidad de almacenamiento interno. Es el compañero perfecto para aquellos que buscan un estilo de vida activo y quieren monitorear su actividad física y bienestar, todo en un dispositivo portátil y fácil de usar.",
+    cantidad: 1,
+    img: "./assets/samsung/reloj/1.png",
+    alt: "Apple Watch  7 - Dorado "
+}, {
+    id: 20,
+    nombre: "Apple Watch  7 - Plateado",
+    precio: 77000,
+    stock: 4,
+    descripcion: "El Apple Watch 7 es un reloj inteligente de última generación con un diseño elegante y moderno y una amplia variedad de características mejoradas, incluyendo una pantalla más grande y brillante, mayor duración de la batería, resistencia al agua mejorada, y mayor capacidad de almacenamiento interno. Es el compañero perfecto para aquellos que buscan un estilo de vida activo y quieren monitorear su actividad física y bienestar, todo en un dispositivo portátil y fácil de usar.",
+    cantidad: 1,
+    img: "./assets/samsung/reloj/2.png",
+    alt: "Apple Watch  7 - Plateado"
+}, {
+    id: 21,
+    nombre: "Apple Watch  7 - Negro ",
+    precio: 77000,
+    stock: 9,
+    descripcion: "El Apple Watch 7 es un reloj inteligente de última generación con un diseño elegante y moderno y una amplia variedad de características mejoradas, incluyendo una pantalla más grande y brillante, mayor duración de la batería, resistencia al agua mejorada, y mayor capacidad de almacenamiento interno. Es el compañero perfecto para aquellos que buscan un estilo de vida activo y quieren monitorear su actividad física y bienestar, todo en un dispositivo portátil y fácil de usar.",
+    cantidad: 1,
+    img: "./assets/samsung/reloj/3.png",
+    alt: "Apple Watch  7 - Negro "
+}, {
+    id: 22,
+    nombre: "Apple iPad Pro 11",
+    precio: 400000,
+    stock: 10,
+    descripcion: "El iPad Pro es la tablet perfecta para aquellos que buscan un dispositivo de alta calidad y rendimiento. Con su pantalla de alta resolución, procesador avanzado y compatibilidad con el Apple Pencil y el Magic Keyboard, el iPad Pro ofrece una experiencia de productividad y entretenimiento de primer nivel. Ya sea para trabajar, estudiar o simplemente disfrutar de juegos y contenido multimedia, el iPad Pro es el dispositivo portátil ideal para llevar a cualquier lugar.",
+    cantidad: 1,
+    img: "./assets/samsung/tablet/1.png",
+    alt: "Apple iPad Pro 11"
+}, {
+    id: 23,
+    nombre: "Apple iPad 9 gen 10.2 ",
+    precio: 225000,
+    stock: 13,
+    descripcion: "El Apple iPad 9 gen 10.2 es una tablet asequible pero poderosa que ofrece una experiencia de usuario excepcional. Con una pantalla Retina de 10.2 pulgadas, un chip A13 Bionic y conectividad Wi-Fi y LTE, el iPad 9 gen es capaz de manejar fácilmente las tareas diarias y el entretenimiento, mientras que su duradera batería permite un uso prolongado. Además, es compatible con el Apple Pencil y el Smart Keyboard, lo que lo convierte en una herramienta de productividad versátil para el trabajo y el estudio.",
+    cantidad: 1,
+    img: "./assets/samsung/tablet/3.webp",
+    alt: "VAPORESSO  Luxe II 220W"
+}, {
+    id: 24,
+    nombre: "Apple iPad mini 6 gen 8.3",
+    precio: 250000,
+    stock: 2,
+    descripcion: "El Apple iPad mini 6 gen 8.3 es una tablet compacta pero potente que ofrece una experiencia de usuario impresionante. Con una pantalla Liquid Retina de 8.3 pulgadas, un chip A15 Bionic y conectividad Wi-Fi y LTE, el iPad mini 6 es capaz de manejar fácilmente las tareas diarias y el entretenimiento, mientras que su diseño ultraportátil lo hace perfecto para llevar a cualquier lugar. Además, es compatible con el Apple Pencil de segunda generación y es compatible con el Smart Keyboard, lo que lo convierte en una herramienta de productividad versátil para el trabajo y el estudio.",
+    cantidad: 1,
+    img: "./assets/samsung/tablet/2.png",
+    alt: "Apple iPad mini 6 gen 8.3"
+}
+]
+
+const arrEnFormatoJSON = JSON.stringify(listaProductos)
+
+localStorage.setItem("listaProductos", arrEnFormatoJSON)
---
+++ js/app.js
@@ -0,0 +1,243 @@
+class ProductoController {
+    constructor() {
+      this.listaProductos = [];
+    }
+  
+    levantar() {
+      let obtenerListaJSON = localStorage.getItem("listaProductos");
+  
+      if (obtenerListaJSON) {
+        this.listaProductos = JSON.parse(obtenerListaJSON);
+       
+      }
+     
+    }
+  
+    mostrarEnDOM(contenedor_productos) {
+      //limpio el dom
+      contenedor_productos.innerHTML = "";
+      //muestra  la lista completa
+      this.listaProductos.forEach((producto) => {
+        contenedor_productos.innerHTML += `
+              <div class="card" style="width: 18rem;">
+                  <img src="${producto.img}" class="card-img-top" alt="${producto.alt}">
+                  <div class="card-body">
+                      <h5 class="card-title">${producto.nombre}</h5>
+                      <p class="card-text">
+                          ${producto.descripcion}
+                      </p>
+                      <p class="card-text">
+                          $${producto.precio}
+                      </p>
+                      <a href="#" class="btn btn-primary" id="vaper${producto.id}">Añadir al carrito</a>
+                  </div>
+              </div>
+              `;
+      });
+    }
+  }
+  
+  class CarritoController {
+    constructor() {
+      this.listaCarrito = [];
+      (this.precio = document.getElementById("precio")),
+        (this.precio_con_iva = document.getElementById("precio_con_iva"));
+    }
+  
+    borrar(producto) {
+      let indice = this.listaCarrito.indexOf(producto);
+      this.listaCarrito.splice(indice, 1);
+    }
+  
+    levantar() {
+      let obtenerListaJSON = localStorage.getItem("listaCarrito");
+  
+      if (obtenerListaJSON) {
+        this.listaCarrito = JSON.parse(obtenerListaJSON);
+        return true
+      }
+      return false
+    }
+  
+    anadir(producto) {
+      let existeProducto = this.listaCarrito.some(
+        (elemento) => elemento.id == producto.id
+      );
+  
+      if (existeProducto) {
+        const productoEncontrado = this.buscar(producto.id);
+        productoEncontrado.cantidad += 1;
+      } else {
+        this.listaCarrito.push(producto);
+      }
+  
+      let arrFormatoJSON = JSON.stringify(this.listaCarrito);
+      localStorage.setItem("listaCarrito", arrFormatoJSON);
+    }
+  
+    mostrarEnDOM(contenedor_carrito) {
+      //limpio el container
+      contenedor_carrito.innerHTML = "";
+      //muestro todo
+      this.listaCarrito.forEach((producto) => {
+        contenedor_carrito.innerHTML += `
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${producto.img}" class="img-fluid rounded-start" alt="${producto.alt}">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bolder border-bottom-0">${producto.nombre}</h5>
+                            <p class="card-text">${producto.descripcion}</p>
+                            <p class="card-text">cantidad: x ${producto.cantidad} unidades</p>
+                            <p class="card-text">$${producto.precio}</p>
+                            <button id="borrar${producto.id}" class="buttonTrash"><i class="fas fa-trash-alt"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+        document
+          .getElementById(`borrar${producto.id}`)
+          .addEventListener("click", () => {
+            // borramos el producto de this.listaProductos
+            this.borrar(producto);
+            //  actualizamos el storage
+            localStorage.setItem(
+              "listaCarrito",
+              JSON.stringify(this.listaCarrito)
+            );
+            // actualizamos el dom
+            this.mostrarEnDOM(contenedor_carrito);
+            this.mostrarPreciosEnDOM();
+          });
+        document
+          .getElementById(`borrar${producto.id}`)
+          .addEventListener("click", () => {
+            this.borrar(producto);
+          });
+      });
+    }
+  
+    mostrarPreciosEnDOM() {
+      this.precio.innerHTML = "$"+this.calcularTotal();
+      this.precio_con_iva.innerHTML = "$"+this.calcularPrecioConIva();
+    }
+  
+    calcularTotal() {
+      return this.listaCarrito.reduce(
+        (acumulador, producto) =>
+          acumulador + producto.precio * producto.cantidad,
+        0
+      );
+    }
+    
+    calcularPrecioConIva() {
+      const total = this.calcularTotal() * 1.21;
+      return Number(total.toFixed());
+    }
+  
+    buscar(id) {
+      return this.listaCarrito.find((producto) => producto.id == id);
+    }
+  
+    limpiar() {
+      localStorage.removeItem("listaCarrito");
+      this.listaCarrito = [];
+  
+      this.listaCarrito.forEach((producto) => {
+        document
+          .getElementById(`borrar${producto.id}`)
+          .addEventListener("click", () => {
+            // borramos el producto de this.listaProductos
+            this.borrar(producto);
+            //  actualizamos el storage
+            localStorage.setItem(
+              "listaCarrito",
+              JSON.stringify(this.listaCarrito)
+            );
+            // actualizamos el dom
+            this.mostrarEnDOM(contenedor_carrito);
+          });
+      });
+    }
+  }
+  
+  //creando instancias
+  const controladorProductos = new ProductoController();
+  const controladorCarrito = new CarritoController();
+  
+  //Verificar STORAGE
+  controladorProductos.levantar();
+  const levantoAlgo = controladorCarrito.levantar();
+
+  //DOM
+  
+  const finalizar_compra = document.getElementById("finalizar_compra");
+  const vaciar_carrito = document.getElementById("vaciar_carrito");
+  
+  if (levantoAlgo) {
+    controladorCarrito.mostrarPreciosEnDOM(precio, precio_con_iva);
+  }
+  
+  //APP JS
+  controladorProductos.mostrarEnDOM(contenedor_productos);
+  controladorCarrito.mostrarEnDOM(contenedor_carrito);
+  
+  //Añadimos Eventos a los botones de cada CARD
+  
+  controladorProductos.listaProductos.forEach((producto) => {
+    const productoEnDOM = document.getElementById(`vaper${producto.id}`);
+  
+    productoEnDOM.addEventListener("click", () => {
+      controladorCarrito.anadir(producto);
+  
+      controladorCarrito.levantar();
+  
+      controladorCarrito.mostrarEnDOM(contenedor_carrito);
+      controladorCarrito.mostrarPreciosEnDOM(precio, precio_con_iva);
+  
+      Toastify({
+        text: "Producto añadido con éxito",
+        duration: 3000,
+        gravity: "bottom",
+        position: "right",
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+      }).showToast();
+    });
+  });
+  
+  finalizar_compra.addEventListener("click", () => {
+    if (controladorCarrito.listaCarrito.length > 0) {
+      controladorCarrito.limpiar();
+      controladorCarrito.mostrarEnDOM(contenedor_carrito);
+      controladorCarrito.mostrarPreciosEnDOM(precio, precio_con_iva);
+  
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Compra realizada con éxito!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Debe agregar al menos un producto al carrito.",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    }
+  });
+  
+  vaciar_carrito.addEventListener("click", () => {
+    controladorCarrito.limpiar();
+    controladorCarrito.mostrarEnDOM(contenedor_carrito);
+    controladorCarrito.mostrarPreciosEnDOM(precio, precio_con_iva);
+  });
+  
+  
